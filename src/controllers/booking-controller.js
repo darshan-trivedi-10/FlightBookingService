2160406c2f076970e4488dfaa02345a0e5db6b52
Darshan Trivedi <trivedidarshan30@gmail.com>
2023-03-06 20:56:19 +0530

    Added Message Queue Using RabitMQ

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -3,25 +3,46 @@ const { StatusCodes } = require('http-status-codes');
 
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-    try {
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json({
-            message: 'Successfully completed a booking',
-            success: true,
-            err: {},
-            date: response
-        })
-    } catch (error) {
-        return res.status(error.statusCode).json({
-            message: error.message,
-            success: false,
-            err: error.explanation,
-            data: {}
-        })
+const { createChannel, publishMessage } = require('../utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('../config/serverConfig');
+
+class BookingController {
+
+    async sendMessageToQueue(req, res) {
+        try {
+            const channel = await createChannel();
+            const data = { message: 'Success' };
+            await publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+            return res.status(200).json({
+                message: 'Succesfully published the message'
+            })
+        } catch (error) {
+            console.log(error);
+            throw {error};
+        }
+    }
+
+    async create(req, res) {
+        try {
+            const response = await bookingService.createBooking(req.body);
+            return res.status(StatusCodes.OK).json({
+                message: 'Successfully completed a booking',
+                success: true,
+                err: {},
+                date: response
+            })
+        } catch (error) {
+            return res.status(error.statusCode).json({
+                message: error.message,
+                success: false,
+                err: error.explanation,
+                data: {}
+            })
+        }
     }
 }
 
-module.exports = {
-    create
-}
+module.exports = BookingController;
+
+
+module.exports = BookingController;
